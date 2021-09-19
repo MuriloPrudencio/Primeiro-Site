91d2ee3649ed8a10514ddda43538a4cf39b80e02
MuriloPrudencio <90528471+MuriloPrudencio@users.noreply.github.com>
2021-09-19 18:44:49 -0300

    Services

--- js/main.js
+++ js/main.js
@@ -5,3 +5,8 @@ $('.slider-principal').slick({
     slidesToShow: 1,
     adaptiveHeight: true
 });
+$('.slider-service').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  }); 
